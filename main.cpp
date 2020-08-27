3f4b8f20ee321741f7c09bab9c419ceac460a961
Gregg <greggory.png@gmail.com>
2020-08-26 21:59:12 -0700

    8/25/2020

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 /*
-This is a text based adventure game called "Freddie Mercury Simulator 2020". In it, you play as Freddie Mercury, a famous singer with a secret power to turn into a cat. He must cure himself of food poisoning before his concert in one hour, or else he will puke during the concert!
+This is a text-based adventure game in which Irene has to battle everyone
 @author Greggory Hickman, January 2020
 @version dev
- */
+*/
 
 #include <iostream>
 #include <cstring>
@@ -14,9 +14,9 @@ This is a text based adventure game called "Freddie Mercury Simulator 2020". In 
 using namespace std;
 
 int main() {
-  
+
   //Greeting the user for the first time
-  cout << "Hello, and welcome to Freddie Mercury Simulator 2020!" << endl << endl << "You are Freddie Mercury, and you have a concert in one hour," << endl << "but the cursed poutine you had for lunch gave you food poisoning!!!" << endl << "If you still have food poisoning during your concert, you will throw up on stage." << endl << "From a small child, you have owned several cats, and have learned their secrets. Because of this, you are able to transform into a cat at will." << endl << endl << "Objective:" << endl << "You must find a way to cure your food poisoning before your concert in one hour." << endl << endl;
+  cout << "Hello, and welcome to Irene Simulator 2020!" << endl << endl << "You are Freddie Mercury, and you have a concert in one hour," << endl << "but the cursed poutine you had for lunch gave you food poisoning!!!" << endl << "If you still have food poisoning during your concert, you will throw up on stage." << endl << "From a small child, you have owned several cats, and have learned their secrets. Because of this, you are able to transform into a cat at will." << endl << endl << "Objective:" << endl << "You must find a way to cure your food poisoning before your concert in one hour." << endl << endl;
   cout << "Commands:" << endl << "move - Moves your character to an adjacent room" << endl << "shift - Transform into a cat" << endl << "quit - Ends the game. Game cannot be saved, so this deletes your progress" << endl << "help - Lists all possible commands" << endl << endl;
 
   bool cat = false;
@@ -24,12 +24,12 @@ int main() {
 
     cout << "Please type your command: " << endl;
     cout << "FMS2020> ";
-    
+
     char* cmd = new char[1000];
     cin >> cmd; cin.clear(); cin.ignore((char)0, 1000);
 
     cout << "-----" << endl << endl;
-    
+
     //If the player typed "move"
     if (strcmp("move", cmd) == 0 || cmd[0] == 'm') {
       cout << endl << "-----" << endl;
